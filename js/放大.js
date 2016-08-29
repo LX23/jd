01b137fcf3ee1df1240a48b33006748d1e60052c
LX23 <164828695@qq.com>
2016-08-29 19:01:03 +0800

    changes

--- js/放大.js
+++ js/放大.js
@@ -1,38 +1,39 @@
 // JavaScript Document
 
 //放大镜----------------------------------------------------------------
+function zoom(id,zoomid){
+	var oDiv=document.getElementById(id);
+	var oMir=oDiv.getElementsByTagName('div')[0];
+	var oBig=document.getElementById(zoomid);
+	var oPic=oBig.getElementsByTagName('img')[0];
+	oDiv.onmousemove=function(ev){
+		var oEv=ev || window.event;
+		var l=oEv.clientX-oDiv.offsetLeft-oMir.offsetWidth/2;
+		var t=oEv.clientY-oDiv.offsetTop-oMir.offsetHeight/2;
+		if(l<0)l=0;
+		if(t<0)t=0;
+		if(l>oDiv.offsetWidth-oMir.offsetWidth)l=oDiv.offsetWidth-oMir.offsetWidth;
+		if(t>oDiv.offsetHeight-oMir.offsetHeight)t=oDiv.offsetHeight-oMir.offsetHeight;
 
-function hxsd_fd(left_box,right_box,bigpic,ospan){
-	left_box.onmouseover=function(){
-				ospan.style.display='block';
-				right_box.style.display='block';
-			};
-		left_box.onmouseout=function(){
-			ospan.style.display='';
-			right_box.style.display='';
-		};
-		left_box.onmousemove=function(ev){
-			var oEv=ev||window.event;
-			
-			// l =鼠标X坐标-父盒子左边的距离-span盒子宽度的一半
-			var l=oEv.clientX-left_box.offsetLeft-ospan.offsetWidth/2;
-			var t=oEv.clientY-left_box.offsetTop-ospan.offsetHeight/2;
-			
-			if(l<0)l=0;
-			if(t<0)t=0;
-			
-			if(l>left_box.offsetWidth-ospan.offsetWidth) l=left_box.offsetWidth-ospan.offsetWidth;
-			if(t>left_box.offsetHeight-ospan.offsetHeight) t=left_box.offsetHeight-ospan.offsetHeight;
-			
-			var l_rate=l/(left_box.offsetWidth-ospan.offsetWidth);
-			var t_rate=t/(left_box.offsetHeight-ospan.offsetHeight);
-			//
-			bigpic.style.left=-(bigpic.offsetWidth-right_box.offsetWidth)*l_rate+"px";
-			bigpic.style.top=-(bigpic.offsetHeight-right_box.offsetHeight)*t_rate+"px";
-			
-			ospan.style.left=l+"px";
-			ospan.style.top=t+"px";
+		oMir.style.left=l+"px";
+		oMir.style.top=t+"px";
+		var rate_l=oMir.offsetLeft/(oDiv.offsetWidth-oMir.offsetWidth);
+		var rate_t=oMir.offsetTop/(oDiv.offsetHeight-oMir.offsetHeight);
+		var l_pic=oPic.offsetWidth-oBig.offsetWidth;
+		var t_pic=oPic.offsetHeight-oBig.offsetHeight;
+		oPic.style.left=-rate_l*l_pic+'px';
+		oPic.style.top=-rate_t*t_pic+'px';
 	};
-};
+
+
+	oDiv.onmouseover=function(){
+		oMir.style.display=oBig.style.display="block";
+	};
+
+	oDiv.onmouseout=function(){
+		oMir.style.display=oBig.style.display="none";
+	};
+}
+
 
 
